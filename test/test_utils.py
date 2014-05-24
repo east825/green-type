3e1d4605ea72e5b7012c33a595c5f8db218fe41c
Mikhail Golubev <qsolo825@gmail.com>
2014-05-25 02:44:58 +0400

    Use capsys builtin py.test fixture to check stdout content in timed tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,13 +1,6 @@
 import time
-import sys
 
 from greentype import utils
-from greentype.utils import PY2
-
-if PY2:
-    from StringIO import StringIO
-else:
-    from io import StringIO
 
 def test_camel_to_snake():
     assert utils.camel_to_snake('') == ''
@@ -49,42 +42,34 @@ def test_qname():
     assert utils.qname_split('foo') == (None, 'foo')
     assert utils.qname_split('') == (None, '')
 
-def test_timed():
-    stream = StringIO()
-    old_stdout = stream
-    sys.stdout = old_stdout
-    try:
-        # use case #1: decorator with custom message
-        @utils.timed_function(msg='msg #1')
-        def func(n):
-            time.sleep(n)
 
-        func(0.1)
+def test_timed(capsys):
+    # use case #1: decorator with custom message
+    @utils.timed_function(msg='msg #1')
+    def func(n):
+        time.sleep(n)
 
-        # use case #2: decorator with standard message
-        @utils.timed_function
-        def func(n):
-            time.sleep(n)
+    func(0.1)
+    assert capsys.readouterr()[0] == 'msg #1: 0.10s\n'
 
-        func(0.1)
+    # use case #2: decorator with standard message
+    @utils.timed_function
+    def func(n):
+        time.sleep(n)
 
-        # use case #3: pass both header and function
-        utils.timed_function(time.sleep, 'msg #3')(0.1)
+    func(0.1)
+    assert capsys.readouterr()[0] == 'Total: 0.10s\n'
 
-        # use case #4: context manager
-        with utils.timed(msg='msg #4'):
-            time.sleep(0.1)
+    # use case #3: pass both header and function
+    utils.timed_function(time.sleep, 'msg #3')(0.1)
+    assert capsys.readouterr()[0] == 'msg #3: 0.10s\n'
 
-        # use case #5: supplied callable and arguments
-        utils.timed('msg #5', time.sleep, (0.1,))
+    # use case #4: context manager
+    with utils.timed(msg='msg #4'):
+        time.sleep(0.1)
+    assert capsys.readouterr()[0] == 'msg #4: 0.10s\n'
 
-        lines = sys.stdout.getvalue().splitlines()
-        assert lines[0] == 'msg #1: 0.10s'
-        assert lines[1] == 'Total: 0.10s'
-        assert lines[2] == 'msg #3: 0.10s'
-        assert lines[3] == 'msg #4: 0.10s'
-        assert lines[4] == 'msg #5: 0.10s'
-
-    finally:
-        sys.stdout = old_stdout
+    # use case #5: supplied callable and arguments
+    utils.timed('msg #5', time.sleep, (0.1,))
+    assert capsys.readouterr()[0] == 'msg #5: 0.10s\n'
 
